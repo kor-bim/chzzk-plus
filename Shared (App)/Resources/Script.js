f0b48992b0bf19c57b45d5719e9748483266a79c
kor-bim <me@hanbin.dev>
2026-07-18 01:21:34 +0900

    style(app): 시작 화면을 간결하게 정리해

--- Shared (App)/Resources/Script.js
+++ Shared (App)/Resources/Script.js
@@ -2,10 +2,9 @@ function show(platform, enabled, useSettingsInsteadOfPreferences) {
     document.body.classList.add(`platform-${platform}`);
 
     if (useSettingsInsteadOfPreferences) {
-        document.getElementsByClassName('platform-mac state-on')[0].innerText = "chzzk-plus’s extension is currently on. You can turn it off in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-off')[0].innerText = "chzzk-plus’s extension is currently off. You can turn it on in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "You can turn on chzzk-plus’s extension in the Extensions section of Safari Settings.";
-        document.getElementsByClassName('platform-mac open-preferences')[0].innerText = "Quit and Open Safari Settings…";
+        document.getElementsByClassName('platform-mac state-off')[0].innerText = "확장 프로그램이 꺼져 있습니다. Safari 설정에서 활성화해 주세요.";
+        document.getElementsByClassName('platform-mac state-unknown')[0].innerText = "Safari 설정의 확장 프로그램에서 CHZZK Plus를 켜주세요.";
+        document.getElementsByClassName('platform-mac open-preferences')[0].innerText = "Safari 확장 프로그램 설정 열기";
     }
 
     if (typeof enabled === "boolean") {
